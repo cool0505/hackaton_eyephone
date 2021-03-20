1358809630466b9a9596bb674b6f7ab953589528
cool0505 <joowonkoh@naver.com>
2021-03-20 14:56:02 +0900

    relationship

--- eyephone_project/app/src/main/java/com/example/eyephone_project/MainActivity.java
+++ eyephone_project/app/src/main/java/com/example/eyephone_project/MainActivity.java
@@ -18,67 +18,38 @@ import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
     private Button btnStart;
     private TextView Title;
-    FirebaseDatabase database;
-    DatabaseReference bbsRef;
     ListViewAdapter adapter;
-    List<Bbs> datas = new ArrayList<>();
+    Dao dao=new Dao();
     String[] str=new String[5];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        database = FirebaseDatabase.getInstance();
-        bbsRef = database.getReference("news");
-        bbsRef.addListenerForSingleValueEvent(postListener);
         btnStart = (Button) findViewById(R.id.btnStart);
         Title = (TextView) findViewById(R.id.Title);
-        System.out.println(datas.size());
+        dao.LoadDao();
         btnStart.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
                 //Intent intent = new Intent(MainActivity.this, ReadNewsActivity.class);
                 Intent intent = new Intent(MainActivity.this, ListActivity.class);
-                System.out.println(datas.size());
+                System.out.println(dao.datas.size());
                 for(int i=0;i<5;i++) {
-                    Bbs b=datas.get(i);
+                    Bbs b=dao.datas.get(i);
                     str[i]=b.title;
                 }
                 intent.putExtra("title", str);
+                //intent.putExtra("key", str);
                 startActivity(intent);
             }
         });
 
     }
-    ValueEventListener postListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            // 위에 선언한 저장소인 datas를 초기화하고
-
-            datas.clear();
-            // bbs 레퍼런스의 스냅샷을 가져와서 레퍼런스의 자식노드를 바복문을 통해 하나씩 꺼내서 처리.
-            for( DataSnapshot snapshot : dataSnapshot.getChildren() ) {
-                String key  = snapshot.getKey();
-                System.out.println(key);
-
-                Bbs bbs = snapshot.getValue(Bbs.class); // 컨버팅되서 Bbs로........
-                bbs.key = key;
-                datas.add(bbs);
-
-            }
-            System.out.println(datas.size());
-            //adapter.notifyDataSetChanged();
-        }
-
-        @Override
-        public void onCancelled(DatabaseError databaseError) {
-            // Getting Post failed, log a message
-            Log.w("MainActivity", "loadPost:onCancelled", databaseError.toException());
-            // ...
-        }
-    };
 }
